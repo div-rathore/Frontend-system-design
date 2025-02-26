fdcf42b546c40369e2ea2836277457b84514e7d0
div-rathore <divyanshurathore11@gmail.com>
2025-02-26 20:54:36 +0530

    e2e testing

--- Testing/e2e.js
+++ Testing/e2e.js
@@ -4,7 +4,7 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({
     headless: false,
     slowMo: 100,
-    args: ["--window-size=1440,900"]
+    args: ["--window-size=1440,900"],
   });
 
   const page = await browser.newPage();
@@ -12,19 +12,21 @@ const puppeteer = require("puppeteer");
   await page.goto("https://namastedev.com/");
 
   console.log("webpage loaded");
-  await page.setViewport({width:1440, height:900});
+  await page.setViewport({ width: 1440, height: 900 });
 
-  const coursesPagelink = "body > main > div:nth-child(4) > div > div.relative.w-full.m-0.p-0.overflow-hidden > div.absolute.top-56.w-full.text-center > div.flex.justify-center.gap-4 > span";
+  const coursesPagelink =
+    "body > main > div:nth-child(4) > div > div.relative.w-full.m-0.p-0.overflow-hidden > div.absolute.top-56.w-full.text-center > div.flex.justify-center.gap-4 > span";
 
-  await page.waitForSelector(coursesPagelink)
+  await page.waitForSelector(coursesPagelink);
 
-  await page.click(coursesPagelink)
+  await page.click(coursesPagelink);
 
-  const systemDesign = "body > main > div:nth-child(4) > div.ml-0> div > div.flex.flex-wrap.gap-5 > a:nth-child(2)"
+  const systemDesign =
+    "body > main > div:nth-child(4) > div.ml-0> div > div.flex.flex-wrap.gap-5 > a:nth-child(2)";
 
-  await page.waitForSelector(systemDesign)
+  await page.waitForSelector(systemDesign);
 
-  await page.click(systemDesign)
+  await page.click(systemDesign);
 
-  await browser.close()
+  await browser.close();
 })();
