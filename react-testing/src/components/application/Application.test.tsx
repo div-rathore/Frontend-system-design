9fefb59116e4460f725e66529dd775cf4aac8fca
div-rathore <divyanshurathore11@gmail.com>
2025-03-28 23:35:06 +0530

    getByRole options and getByLabelText

--- react-testing/src/components/application/Application.test.tsx
+++ react-testing/src/components/application/Application.test.tsx
@@ -1,16 +1,36 @@
-import { render, screen } from "@testing-library/react";
+import { getByRole, render, screen } from "@testing-library/react";
 import { Application } from "./Application";
 
-describe('application', ()=>{
-    test('renders correctly', ()=>{
-        render(<Application/>);
-       const nameElement =  screen.getByPlaceholderText('Fullname');
-       expect(nameElement).toBeInTheDocument();
+describe("application", () => {
+  test("renders correctly", () => {
+    render(<Application />);
 
-       const jobLocationElement = screen.getByRole('combobox')
-       expect(jobLocationElement).toBeInTheDocument()
+    const pageHeading = screen.getByRole("heading", {
+      level: 1,
+    });
+    expect(pageHeading).toBeInTheDocument();
+    const sectionHeading = screen.getByRole("heading", {
+      level: 2,
+    });
+    expect(sectionHeading).toBeInTheDocument();
+    const nameElement = screen.getByRole("textbox", {
+      name: "Name",
+    });
 
-       const submitButtonElement = screen.getByRole('button')
-       expect(submitButtonElement).toBeInTheDocument()
-    })
-})
+    const nameElement2 = screen.getByLabelText("Name");
+    expect(nameElement2).toBeInTheDocument();
+    expect(nameElement).toBeInTheDocument();
+
+    const bioElement = screen.getByRole("textbox", {
+      name: "Bio",
+    });
+
+    expect(bioElement).toBeInTheDocument();
+
+    const jobLocationElement = screen.getByRole("combobox");
+    expect(jobLocationElement).toBeInTheDocument();
+
+    const submitButtonElement = screen.getByRole("button");
+    expect(submitButtonElement).toBeInTheDocument();
+  });
+});
